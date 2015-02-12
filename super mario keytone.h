69af150bf2510a596aa1c031a6f1564ebd889a4f
Colin Borins <colinborins@hotmail.com>
2015-02-12 03:22:12 -0500

    Update super mario keytone.h

--- super mario keytone.h
+++ super mario keytone.h
@@ -1,114 +1,109 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1344\cocoasubrtf720
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural
+ardirnatural
 
-\f0\fs24 \cf0 \
-#include "pitches.h"\
-\
-int melody[] = \
-\{\
-NOTE_E7, NOTE_E7, 0, NOTE_E7,\
-  0, NOTE_C7, NOTE_E7, 0,\
-  NOTE_G7, 0, 0,  0,\
-  NOTE_G6, 0, 0, 0,\
- \
-  NOTE_C7, 0, 0, NOTE_G6,\
-  0, 0, NOTE_E6, 0,\
-  0, NOTE_A6, 0, NOTE_B6,\
-  0, NOTE_AS6, NOTE_A6, 0,\
-\
-  NOTE_G6, NOTE_E7, NOTE_G7,\
-  NOTE_A7, 0, NOTE_F7, NOTE_G7,\
-  0, NOTE_E7, 0,NOTE_C7,\
-  NOTE_D7, NOTE_B6, 0, 0,\
-\
-  NOTE_C7, 0, 0, NOTE_G6,\
-  0, 0, NOTE_E6, 0,\
-  0, NOTE_A6, 0, NOTE_B6,\
-  0, NOTE_AS6, NOTE_A6, 0,\
-\
-  NOTE_G6, NOTE_E7, NOTE_G7,\
-  NOTE_A7, 0, NOTE_F7, NOTE_G7,\
-  0, NOTE_E7, 0,NOTE_C7,\
-  NOTE_D7, NOTE_B6, 0, 0,\
- \
-  NOTE_E7, NOTE_E7, 0, NOTE_E7,\
-  0, NOTE_C7, NOTE_E7, 0,\
-  NOTE_G7, 0, 0,  0,\
-  NOTE_G6, 0, 0, 0,NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4, \
-  NOTE_AS3, NOTE_AS4, 0,\
+#include "pitches.h"
+
+int melody[] = 
+{
+NOTE_E7, NOTE_E7, 0, NOTE_E7,
+  0, NOTE_C7, NOTE_E7, 0,
+  NOTE_G7, 0, 0,  0,
+  NOTE_G6, 0, 0, 0,
+ 
+  NOTE_C7, 0, 0, NOTE_G6,
+  0, 0, NOTE_E6, 0,
+  0, NOTE_A6, 0, NOTE_B6,
+  0, NOTE_AS6, NOTE_A6, 0,
+
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+  0, NOTE_E7, 0,NOTE_C7,
+  NOTE_D7, NOTE_B6, 0, 0,
+
+  NOTE_C7, 0, 0, NOTE_G6,
+  0, 0, NOTE_E6, 0,
+  0, NOTE_A6, 0, NOTE_B6,
+  0, NOTE_AS6, NOTE_A6, 0,
+
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+  0, NOTE_E7, 0,NOTE_C7,
+  NOTE_D7, NOTE_B6, 0, 0,
+ 
+  NOTE_E7, NOTE_E7, 0, NOTE_E7,
+  0, NOTE_C7, NOTE_E7, 0,
+  NOTE_G7, 0, 0,  0,
+  NOTE_G6, 0, 0, 0,NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4, 
+  NOTE_AS3, NOTE_AS4, 0,
   0,\
-  NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4, \
-  NOTE_AS3, NOTE_AS4, 0,\
-  0,\
-  NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,\
-  NOTE_DS3, NOTE_DS4, 0,\
-  0,\
-  NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,\
-  NOTE_DS3, NOTE_DS4, 0,\
-  0, NOTE_DS4, NOTE_CS4, NOTE_D4,\
-  NOTE_CS4, NOTE_DS4, \
-  NOTE_DS4, NOTE_GS3,\
-  NOTE_G3, NOTE_CS4,\
-  NOTE_C4, NOTE_FS4,NOTE_F4, NOTE_E3, NOTE_AS4, NOTE_A4,\
-  NOTE_GS4, NOTE_DS4, NOTE_B3,\
-  NOTE_AS3, NOTE_A3, NOTE_GS3,\
-  0, 0, 0\
-\};\
-\
+  NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4, 
+  NOTE_AS3, NOTE_AS4, 0,
+  0,
+  NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
+  NOTE_DS3, NOTE_DS4, 0,
+  0,
+  NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
+  NOTE_DS3, NOTE_DS4, 0,
+  0, NOTE_DS4, NOTE_CS4, NOTE_D4,
+  NOTE_CS4, NOTE_DS4, 
+  NOTE_DS4, NOTE_GS3,
+  NOTE_G3, NOTE_CS4,
+  NOTE_C4, NOTE_FS4,NOTE_F4, NOTE_E3, NOTE_AS4, NOTE_A4,
+  NOTE_GS4, NOTE_DS4, NOTE_B3,
+  NOTE_AS3, NOTE_A3, NOTE_GS3,
+  0, 0, 0
+};
+
 int noteDurations[] = \{\
-  12, 12, 12, 12,\
-  12, 12, 12, 12,\
-  12, 12, 12, 12,\
-  12, 12, 12, 12,\
-\
-  12, 12, 12, 12,\
-  12, 12, 12, 12,\
-  12, 12, 12, 12,\
-  12, 12, 12, 12,\
-\
-  9, 9, 9,\
-  12, 12, 12, 12,\
-  12, 12, 12, 12,\
-  12, 12, 12, 12,\
-\
-  12, 12, 12, 12,\
-  12, 12, 12, 12,\
-  12, 12, 12, 12,\
-  12, 12, 12, 12,\
-\
-  9, 9, 9,\
-  12, 12, 12, 12,\
-  12, 12, 12, 12,\
-  12, 12, 12, 12,\
- \
-  12, 12, 12, 12,\
-  12, 12, 12, 12,\
-  12, 12, 12, 12,\
-  12, 12, 12, 12,\
-  \
-  12, 12, 12, 12, \
-  12, 12, 6,\
-  3,\
-  12, 12, 12, 12, \
-  12, 12, 6,\
-  3,\
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  
+  12, 12, 12, 12, 
+  12, 12, 6,
+  3,
+  12, 12, 12, 12, 
+  12, 12, 6,
+  3,
   12, 12, 12, 12, 12, 12, 6,3,12, 12, 12, 12, 12, 12, 6, 6, 18, 18, 18,6, 6,6, 6,6, 6,18, 18, 18,18, 18, 18,10, 10, 10,10, 10, 10,3, 3, 3\
-\};\
-\
-void setup() \{\
-\
+};
+
+void setup() {
+
   for (int thisNote = 0; thisNote < 150; thisNote++) \{\
-\
-   \
+
+   
     int noteDuration = 1000 / noteDurations[thisNote];\
     tone(13, melody[thisNote], noteDuration);\
     int pauseBetweenNotes = noteDuration * 1.40;\
     delay(pauseBetweenNotes);\
     noTone(13);\
-  \}\
-\}\
-\
-void loop() \{ \}}
+  }
+}
+
+void loop() { }}
